3c519e8ebd13ffa16a3563db09352c8e48b860ca
vae97 <gakashekanayaka@gmail.com>
2020-10-17 17:37:11 +0530

    Original Home page created

--- traveltoday/src/Components/div1.js
+++ traveltoday/src/Components/div1.js
@@ -6,33 +6,54 @@ import ExploreIcon from '@material-ui/icons/Explore';
 
 const useStyles = makeStyles((theme) => ({
     container:{
-        height:'500px',
-        backgroundColor:'rgb(0,128,128)'
+        height:'700px',
+        backgroundColor:'white'
     },
     text:{
         backgroundColor:'rgb(0,128,128)',
-        height:'250px',
+        height:'400px',
         textAlign:'center',
-        paddingTop:'100px',
-        padding:'3px'
+
     },
     svg:{
-        backgroundColor:'rgb(0,128,128)',
-        height:'250px',
+        backgroundColor:'white',
+        height:'300px',
         textAlign:'center'
     
     },
     button:{
-        backgroundColor:'rgb(0,128,128)',
-        height:'250px',
+        backgroundColor:'white',
+        height:'300px',
         textAlign:'center',
-        paddingTop:'100px',
 
     },
-    font:{
+    font1:{
         color:'white',
-        fontSize:'30px',
-        
+        fontSize:'24px',
+        fontWeight:'bold',
+        backgroundColor:'rgba(81, 90, 90 ,5%)',
+        width:'90%',
+        margin:'auto',
+        marginTop:'150px',
+        borderRadius:'10px',
+        padding:'2px'
+    },
+    font2:{
+        color:'rgb(246, 248, 248)',
+        fontSize:'14px',
+        fontStyle:'italic',
+        backgroundColor:'rgba(81, 90, 90 ,5%)',
+        width:'90%',
+        margin:'auto',
+        borderRadius:'10px',
+        padding:'2px'
+    },
+    realbutton:{
+        color:'white',
+        backgroundColor:'green',
+        '&:hover': {
+            background: "#f00",
+         },
     }
 }));
 
@@ -42,14 +63,21 @@ export default function Div1() {
   return (
       <Grid container className={classes.container} xs={12}>
           <Grid item className={classes.text} xs={12} >
-              <Typography className={classes.font}>"Find your next Destination in Sri Lanka"</Typography>
+              <Paper className="backgroundimage" >
+                <Typography className={classes.font1}>" Find your next Destination in Sri Lanka "</Typography>
+                <Typography className={classes.font2}>Settle in somewhere new. Discover stays to live, work, or just relax.</Typography>
+              </Paper>
           </Grid>
 
-          <Grid item className={classes.svg} xs={7} md={3}>
-              <Paper className={'image'}></Paper>
+          <Grid item className={classes.svg} xs={6} md={4}>
+              <Paper className="image" elevation={0} square={true}></Paper>
           </Grid>
-          <Grid item className={classes.button} xs={5} md={9}>
-              <Button variant="contained" startIcon={<ExploreIcon/>}>Explore</Button>
+          <Grid item className={classes.button} xs={6} md={8}>
+              <Paper elevation={0} square={true} className="explore">
+                <div className='div'>
+                    <Button variant="contained" className={classes.realbutton} startIcon={<ExploreIcon/>}>Explore</Button>
+                </div>
+              </Paper>
           </Grid>
       </Grid>
   );
