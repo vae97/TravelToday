8ba8cdd5d83fab8b5eccf70f16a65b0aef0d6fb2
vae97 <gakashekanayaka@gmail.com>
2020-10-22 00:33:05 +0530

    sign in div created

--- traveltoday/src/App.js
+++ traveltoday/src/App.js
@@ -3,6 +3,7 @@ import NavigationBar from './Components/navbar';
 import Div1 from './Components/div1'
 import Div2 from './Components/div2'
 import Div3 from './Components/div3'
+import Div4 from './Components/div4'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Div1/>
       <Div2/>
       <Div3/>
+      <Div4/>
     </div>
   );
 }
