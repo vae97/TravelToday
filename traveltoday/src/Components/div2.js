8bc7b9e68fb1354e1865bf5dbedd2edaf5d5cf0f
vae97 <gakashekanayaka@gmail.com>
2020-10-21 18:20:17 +0530

    home page with three divs

---
+++ traveltoday/src/Components/div2.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid, Typography} from '@material-ui/core';
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        height:'300px',
+        textAlign:'center'
+    },
+    text:{
+        typography: {
+            "fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
+            "fontSize": 14,
+            "fontWeightLight": 300,
+            "fontWeightRegular": 400,
+            "fontWeightMedium": 500
+           },
+        margin:'auto',
+        fontSize:'35px',
+        paddingTop:'50px'
+    }
+
+    
+}));
+
+export default function Div2() {
+  const classes = useStyles();
+
+  return (
+      <Grid container className={classes.root} xs={12}>
+          <Grid item xs={12} md={12}>
+              <Typography className={classes.text}>" Top destinations for Sri Lanka city trips "</Typography>
+          </Grid>
+      </Grid>
+  );
+}
